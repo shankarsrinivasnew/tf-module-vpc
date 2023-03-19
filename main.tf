934cdaf450652e2191cec5bc04f0f5f398f3b034
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-03-19 22:31:02 +0530

    sdf

--- main.tf
+++ main.tf
@@ -2,3 +2,24 @@ resource "aws_vpc" "main_vpc_r" {
     cidr_block = var.vpc_cidr
     tags = merge( var.tags, { Name = "${var.env}-vpc" }) 
 }
+
+resource "aws_subnet" "public_subnets_r" {
+  vpc_id     = aws_vpc.main_vpc_r.id
+  for_each = var.public_subnets
+  cidr_block = each.value ["cidr_block"]
+  availability_zone = each.value ["availability_zone"]
+  tags = merge ( var.tags,
+    { Name = "${var.env}-${each.value["name"]}" }
+    )
+}
+
+resource "aws_subnet" "private_subnets_r" {
+  vpc_id     = aws_vpc.main_vpc_r.id
+  for_each = var.private_subnets
+  cidr_block = each.value ["cidr_block"]
+  availability_zone = each.value ["availability_zone"]
+  tags = merge ( var.tags,
+    { Name = "${var.env}-${each.value["name"]}" }
+    )
+}
+
