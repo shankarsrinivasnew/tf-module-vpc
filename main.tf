eebda5bd838d77cb05f3995015f29d875408d5fd
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-03-19 23:23:42 +0530

    dfgg

--- main.tf
+++ main.tf
@@ -29,6 +29,10 @@ resource "aws_route_table" "public_route_table_r" {
   tags = merge(var.tags,
     { Name = "${var.env}-${each.value["name"]}" }
   )
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw_r.id
+  }
 
 }
 
@@ -53,4 +57,10 @@ resource "aws_route_table_association" "private_association" {
   # subnet_id      = aws_subnet.private_subnets_r.each.values["name"].id
   subnet_id      = lookup(lookup(aws_subnet.private_subnets_r, each.value["name"], null), "id", null)
   route_table_id = aws_route_table.private_route_table_r[each.value["name"]].id
+}
+
+
+resource "aws_internet_gateway" "igw_r" {
+  vpc_id   = aws_vpc.main_vpc_r.id
+  tags       = merge(var.tags, { Name = "${var.env}-vpc" })
 }
