c38be8e7660b7695a01cf41d60db5365440d9d3b
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-03-19 23:14:39 +0530

    xcbv

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_subnet" "private_subnets_r" {
 }
 
 resource "aws_route_table" "public_route_table_r" {
-  vpc_id = aws_vpc.main_vpc_r.id
+  vpc_id   = aws_vpc.main_vpc_r.id
   for_each = var.public_subnets
   tags = merge(var.tags,
     { Name = "${var.env}-${each.value["name"]}" }
@@ -33,10 +33,24 @@ resource "aws_route_table" "public_route_table_r" {
 }
 
 resource "aws_route_table" "private_route_table_r" {
-  vpc_id = aws_vpc.main_vpc_r.id
+  vpc_id   = aws_vpc.main_vpc_r.id
   for_each = var.private_subnets
   tags = merge(var.tags,
     { Name = "${var.env}-${each.value["name"]}" }
   )
 
+}
+
+resource "aws_route_table_association" "public_association" {
+  for_each = var.public_subnets
+  # subnet_id      = aws_subnet.public_subnets_r.each.values["name"].id
+  subnet_id      = lookup(lookup(aws_subnet.public_subnets_r, each.value["name"], null), "id", null)
+  route_table_id = aws_route_table.public_route_table_r[each.values["name"]].id
+}
+
+resource "aws_route_table_association" "private_association" {
+  for_each = var.private_subnets
+  # subnet_id      = aws_subnet.private_subnets_r.each.values["name"].id
+  subnet_id      = lookup(lookup(aws_subnet.private_subnets_r, each.value["name"], null), "id", null)
+  route_table_id = aws_route_table.private_route_table_r[each.values["name"]].id
 }
